1aeca8d3937d99eb8dd2394f8f4161ddbf0807c5
Arshad <arshad.syed.anwar@gmail.com>
2022-05-07 01:40:26 +0100

    add single item details

---
+++ src/Pages/SingleItemDetails/SingleItemDetails.js
@@ -0,0 +1,126 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import useProductDetails from "../../hooks/useProductDetails";
+import { useForm } from "react-hook-form";
+import "./SingleItemDetails.css";
+
+const SingleItemDetails = () => {
+  const [quantity, setQuantity] = useState([]);
+  const { id } = useParams();
+  const [product] = useProductDetails(id);
+
+  useEffect(() => {
+    setQuantity(product?.quantity);
+  }, [product]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const url = `${process.env.REACT_APP_BASE_URL}/product/${id}`;
+  const fetchData = async (newData) => {
+    await fetch(url, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newData),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        setQuantity(parseInt(result.quantity));
+      });
+  };
+
+  const onSubmit = (data) => {
+    const newData = { addQuantity: parseInt(data.quantity) };
+    fetchData(newData);
+  };
+
+  const delivered = (_id) => {
+    const newData = { decreaseByOne: 1 };
+    fetchData(newData);
+  };
+
+  return (
+    <>
+      <div className="item-details-page">
+        <h3 className="text-center">Item Details</h3>
+        <div
+          className="item-details-page card"
+          style={{ width: "40%", maxWidth: "36rem" }}
+        >
+          <img src={product?.img} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <p>
+              <strong>Id:</strong> {product?._id}{" "}
+            </p>
+            <p>
+              <strong>Name:</strong> {product?.name}
+            </p>
+            <p>
+              <strong>Description:</strong> {product?.description}
+            </p>
+            <p>
+              <strong>Price: </strong>
+              {product?.price}
+            </p>
+            <p>
+              <strong>Quantity: </strong>
+              {quantity}
+            </p>
+            <p>
+              <strong>Supplier Name: </strong>
+              {product?.supplier}
+            </p>
+            <p>
+              <strong>Quantity Sold: </strong>
+              {product?.quantitySold}
+            </p>
+            <br></br>
+            <div className="d-block text-center">
+              <a
+                href="#!"
+                className="btn mx-auto btn-primary "
+                onClick={() => delivered(id)}
+              >
+                Delivered
+              </a>
+            </div>
+          </div>
+
+          <div className="mx-3">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                  <span class="input-group-text" id="">
+                    Restock
+                  </span>
+                </div>
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Quantity"
+                  aria-label="Recipient's username"
+                  aria-describedby="basic-addon2"
+                  
+                  {...register("quantity", { required: true })}
+                />
+                <div class="input-group-append">
+                  <button class="btn btn-outline-secondary" type="submit">
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </form>
+            {errors.quantity && <span className="text-danger">* Quantity is required</span>}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SingleItemDetails;
